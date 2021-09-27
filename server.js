82e7f1a30a5a3d4c5c9ce1598dec6580d7418b24
jerry <jerry4905@hotmail.com>
2021-09-26 21:48:34 -0700

    heroku server changes

--- server.js
+++ server.js
@@ -14,7 +14,16 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+if (process.env.NODE_ENV === 'production') {
+  //Express to serve up production assets
+  app.use(express.static('client/build'));
+  //Expess will serve up index.html file
+  //if route is not recognized
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
